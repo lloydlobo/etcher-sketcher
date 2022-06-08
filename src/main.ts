3bd4a86dddec1d494a5700bf10fdec41c3a011d3
lloydlobo <lloyd@lloydlobo.com>
2022-06-08 11:33:16 +0530

    style: buttons animation and lfont etter-spacing

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import { sleep } from "./app";
 // endregion:   --- imports ---
 
 // region:      --- app ---
-
 const app = document.querySelector<HTMLDivElement>("#app")!;
-
+const heading1 = `<h1 class="heading">etcher sketcher</h1>`;
+const htmlBtnGetInspired = `<button id="btnGetInspired">get inspired</button>`;
 app.innerHTML = `
-  <h1 class="heading">etcher sketcher</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+${heading1}
+${htmlBtnGetInspired}
 `;
 
 // endregion:   --- app ---
@@ -19,11 +19,31 @@ app.innerHTML = `
 // region:      --- main ---
 export function main() {
   sleep(1000).then(() => {
-    // app.innerHTML = "Hello World!";
-    console.log("Hello World!");
+    console.clear();
+    console.log("Gloria In Excelsis Deo!");
   });
 }
 
 main();
 
 // endregion:   --- main ---
+
+// region:      --- style ---
+
+const styleAddClassBtn = ["shrink-border", "material-bubble"];
+
+// endregion:   --- style ---
+// region:      --- buttons ---
+
+const btnGetInspired = document.getElementById(
+  "btnGetInspired"
+)! as HTMLButtonElement;
+console.dir(btnGetInspired.classList);
+btnGetInspired.classList.add(...styleAddClassBtn);
+// endregion:   --- buttons ---
+
+// region:      --- events ---
+btnGetInspired.addEventListener("click", () => {
+  console.log("get inspired");
+});
+// endregion:   --- events ---
