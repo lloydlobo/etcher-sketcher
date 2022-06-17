c56cf7b140cf3c4ef03cbd147a7af478597c82d9
lloydlobo <lloyd@lloydlobo.com>
2022-06-17 15:06:02 +0530

    chore: format main.test.ts file
    - add more tests there

--- src/main.test.ts
+++ src/main.test.ts
@@ -7,18 +7,18 @@ const window = new Window();
 const document = window.document;
 
 describe('sample test', () => {
-    it('sample test 1+1 should equal 2', () => {
-        expect(1 + 1).toBe(2);
-    });
+  it('sample test 1+1 should equal 2', () => {
+    expect(1 + 1).toBe(2);
+  });
 });
 
 // happy-dom test
 describe('happy-dom test with main', () => {
-    it('document.body.innerHTML should equal "Hello World!"', () => {
-        const body = document.body;
-        body.innerHTML = 'Hello World!';
-        expect(body.innerHTML).toBe('Hello World!');
-    });
+  it('document.body.innerHTML should equal "Hello World!"', () => {
+    const body = document.body;
+    body.innerHTML = 'Hello World!';
+    expect(body.innerHTML).toBe('Hello World!');
+  });
 });
 
 // main test
